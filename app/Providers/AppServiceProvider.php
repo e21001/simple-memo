0a8a913c6e0c6399875525ca4b30c8d46f1f8541
e21001 <e21001@std.it-college.ac.jp>
2022-03-07 21:23:55 +0900

    タグからの検索機能実装

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Memo;
+use App\Models\Tag;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,13 +26,30 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('*', function($view) {
-            $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
+            $query_tag = \Request::query('tag');
+
+            if(!empty($query_tag)){
+                $memos = Memo::select('memos.*')
+                ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                ->where('memo_tags.tag_id', '=', $query_tag)
+                ->where('user_id', '=', \Auth::id())
+                ->whereNull('deleted_at')
+                ->orderBy('updated_at', 'desc')
+                ->get();
+            }else{
+                $memos = Memo::select('memos.*')
+                ->where('user_id', '=', \Auth::id())
+                ->whereNull('deleted_at')
+                ->orderBy('updated_at', 'desc')
+                ->get();
+            }
+
+            $tags = Tag::where('user_id', '=', \Auth::id())
             ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'desc')
+            ->orderBy('id', 'desc')
             ->get();
 
-            $view->with('memos', $memos);
+            $view->with('memos', $memos)->with('tags', $tags);
         });
     }
 }
