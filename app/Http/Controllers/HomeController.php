cd5db1fdecd290d444758a3ff2a5c1d1dae71b3d
e21001 <e21001@std.it-college.ac.jp>
2022-03-07 20:56:16 +0900

    viewComposerで共通処理をまとめる

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,18 +27,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'desc')
-            ->get();
-
         $tags = Tag::where('user_id', '=', \Auth::id())
             ->whereNull('deleted_at')
             ->orderBy('id', 'desc')
             ->get();
 
-        return view('create', compact('memos', 'tags'));
+        return view('create', compact('tags'));
     }
 
     public function store(Request $request)
@@ -83,12 +77,6 @@ class HomeController extends Controller
 
     public function edit($id)
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'desc')
-            ->get();
-
         $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
             ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
             ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
@@ -107,7 +95,7 @@ class HomeController extends Controller
             ->orderBy('id', 'desc')
             ->get();
 
-        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
+        return view('edit', compact('edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
